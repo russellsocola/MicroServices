d76d08541a20e729565843a1dfead1767e61de02
russellsocola <russell999nahum@gamil.com>
2024-08-09 23:13:41 -0500

    Microservice

---
+++ src/main/java/com/socola/product_microservice/Greeting.java
@@ -0,0 +1,29 @@
+package com.socola.product_microservice;
+
+public class Greeting {
+
+    public Long id;
+
+    public String message;
+
+    public Greeting(Long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
